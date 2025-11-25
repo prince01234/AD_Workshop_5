e82e83063db1ecabbb23e34e2ed1d5d563903d87
Prince <princeshrestha2111018@gmail.com>
2025-11-25 20:56:03 +0545

    Task 1 completed

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private double balance;
+
+        public BankAccount(int accountNumber)
+        {
+            this.AccountNumber = accountNumber;
+        }
+
+        public int AccountNumber { get; }
+        public double Balance
+        {
+            get { return Balance; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Balance cannot be negative");
+                }
+                balance = value;
+
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive");
+            }
+            balance += amount;
+            Console.WriteLine($"You have successfully deposited {amount}. New balance is {balance}");
+        }
+
+        public void withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdraw amount must be positive");
+            }
+
+            if (amount > balance)
+            {
+                throw new InvalidOperationException("Insufficinet funds!");
+            }
+            balance -= amount;
+            Console.WriteLine($"You have successfully withdrawn {amount}. New balance is {balance}");
+        }
+    }
+
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(10001);
+            Console.WriteLine($"Account Number: {account.AccountNumber}");
+
+            account.Deposit(5000);
+            account.withdraw(1100);
+        }
+    }
+}
+    
