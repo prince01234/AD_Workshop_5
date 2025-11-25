92c6fbb394dd499588f6f60dec301983ce1eb146
Prince <princeshrestha2111018@gmail.com>
2025-11-25 23:17:13 +0545

    Task 3 completed

---
+++ Task 3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printer
+            Printer printer = new Printer();
+            printer.Print("Merry Christmas!");
+            printer.Print(25);
+            printer.Print("EH...!", 2);
+
+            Console.WriteLine("\n----------------------\n");
+
+            //Nepali 
+            NepaliTeacher nep = new NepaliTeacher();
+            nep.Name = "Prince";
+            Console.WriteLine($"Teacher Name: {nep.Name}");
+            nep.Teaching();
+            nep.SalaryInfo();
+
+            Console.WriteLine();
+
+            //English
+            EnglishTeacher eng = new EnglishTeacher();
+            eng.Name = "Rose";
+            Console.WriteLine($"Teacher Name: {eng.Name}");
+            eng.Teaching();
+            eng.SalaryInfo();
+
+        }
+    }
+}
---
+++ Task 3/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+
+        public virtual void Teaching()
+        {
+            Console.WriteLine("Teacher teaches in English");
+        }
+
+        public void SalaryInfo()
+        {
+            Console.WriteLine("Ask teacher if you want to know salary....");
+        }
+    }
+}
