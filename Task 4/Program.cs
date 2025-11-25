316ed77284e8c57dc2ac9abde05e65b0c7358e40
Prince <princeshrestha2111018@gmail.com>
2025-11-25 23:31:11 +0545

    Task 4 Completed

---
+++ Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bike bike = new Bike();
+
+            Console.WriteLine("Car Actions");
+            car.Display();
+            car.StartEngine();
+            car.StopEngine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Bike Actions");
+            bike.Display();
+            bike.StartEngine();
+            bike.StopEngine();
+
+            Console.WriteLine();
+        }
+    }
+}
